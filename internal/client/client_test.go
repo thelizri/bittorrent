b1a2026f886f8d9e28a46e2e9cfe192a57a0a10d
Daniel Dahlberg <ddahlb@kth.se>
2024-09-14 12:39:01 +0200

    Added tests for verifying the errors thrown by client.StringToClient

---
+++ internal/client/client_test.go
@@ -0,0 +1,27 @@
+package client
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStringToClient(t *testing.T) {
+	failingTests := []struct {
+		addr, expectedError string
+	}{
+		{"178.62.82.89.51470", "address must be in the format IP:Port"},
+		{":51470", "invalid IP address"},
+		{"178.62.82.89:", "invalid port number"},
+		{"178.62.82.89:-1", "port number must be between 0 and 65535"},
+		{"178.62.82.89:65536", "port number must be between 0 and 65535"},
+	}
+
+	for _, tt := range failingTests {
+		t.Run(fmt.Sprintf("%s throws error", tt.addr), func(t *testing.T) {
+			_, err := StringToClient(tt.addr)
+			if err.Error() != tt.expectedError {
+				t.Errorf("Expected error '%s' but none was thrown", tt.expectedError)
+			}
+		})
+	}
+}
